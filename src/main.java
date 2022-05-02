2c6ce8124c842ea648971e47c1a71a4e1bfdc45c
tomasroetti <roettitomas@gmail.com>
2022-05-02 17:57:50 -0300

    EjerciciosJava

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        jugar juegar = new jugar();
+        juegar.juego();
+
+        System.out.println("hola soy tomas");
+
+
+    }
+}
